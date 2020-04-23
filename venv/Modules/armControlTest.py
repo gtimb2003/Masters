69246c898c474990a79b8797234a8f85f90891b7
gtimb2003 <geo_ti@hotmail.com>
2020-04-23 19:51:22 +0300

    Added tinyik to try and solve IK Spoiler alert: "I failed" Used xArm IK. Managed to make manipulator follow a QR code in X Y axis

--- venv/Modules/armControlTest.py
+++ venv/Modules/armControlTest.py
@@ -33,34 +33,25 @@ def inter(Input, InputLow, InputHigh, OutputLow, OutputHigh):
     interpolate = ((Input - InputLow) / (InputHigh - InputLow)) * (OutputHigh - OutputLow) + OutputLow
     return (interpolate)
 
-def interRev(Input, InputLow, InputHigh, OutputLow, OutputHigh):
-    a = ((Input - InputLow) / (InputHigh - InputLow)) * (OutputHigh - OutputLow) + OutputLow
-    interpolate = OutputHigh - a
-    return (interpolate)
-
 
 arm = XArmAPI(ip)
 arm.motion_enable(enable=True)
 arm.set_mode(0)
 arm.set_state(state=0)
-arm.reset(wait=True)
+#arm.reset(wait=True)
 
+#arm.set_servo_angle(angle=[0, 0, 0, 0, 0, 0, 0], speed=100, wait=True)
+#print(arm.get_inverse_kinematics([0, 50, 50, 0 ,0], input_is_radian=False, return_is_radian=False))
 
-while True:
-    # Read the location of the QR Code
-    filepathX = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\xCoor.txt"
-    xFile = open(filepathX, "r+")
-    filepathtY = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\yCoor.txt"
-    yFile = open(filepathtY, "r+")
-    x = int(xFile.read())
-    y = int(yFile.read())
+#arm.set_servo_angle(angle=[0, 98, 0, -83, 0, -164, 0], speed=100, wait=True)
+#a=arm.get_inverse_kinematics([0, 0, 0, 0, 0, 0, 0])
+#b = a[1]
+#c = np.rad2deg(b)
+#print(np.round(c))
 
-    #Interpolate the location of the QR Code
-    xInt = round(inter(x, 137, 1125, 0, 420))
-    yInt = round(inter(y, 110, 650, 450, 700))
+#arm.reset(wait=True)
 
-    print(xInt, yInt)
-    #arm.set_servo_angle(angle=[0, 0, 0, xInt, 0, 0, yInt], speed=100, wait=True)
-    arm.set_position(yInt, xInt, 200, 0, 0, 0, speed=200, wait=True)
-
-    arm.reset(wait=True)
+#move to point A, get IK of PointC at Point A
+arm.set_position(600,0,300,0,0,0,wait=True)
+#IK_pointA_C=arm.get_inverse_kinematics([307,0,112,180,0,0])
+#a=arm.get_inverse_kinematics([300,0,300,0,0,0],False, False)
