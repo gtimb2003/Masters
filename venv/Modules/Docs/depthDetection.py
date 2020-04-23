9d2159878c7435e182df3f71d3b3b143c9dee516
gtimb2003 <geo_ti@hotmail.com>
2020-04-23 21:53:16 +0300

    Added tinyik to try and solve IK Spoiler alert: "I failed" Used xArm IK. Managed to make manipulator follow a QR code in X Y axis

---
+++ venv/Modules/Docs/depthDetection.py
@@ -0,0 +1,28 @@
+import pyrealsense2 as rs
+
+
+# Create a context object. This object owns the handles to all connected realsense devices
+pipeline = rs.pipeline()
+pipeline.start()
+
+def inter(Input, InputLow, InputHigh, OutputLow, OutputHigh):
+    interpolate = ((Input - InputLow) / (InputHigh - InputLow)) * (OutputHigh - OutputLow) + OutputLow
+    return (interpolate)
+
+while True:
+    # This call waits until a new coherent set of frames is available on a device
+    # Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+    frames = pipeline.wait_for_frames()
+    depth = frames.get_depth_frame()
+
+    # Read the location of the QR Code
+    filepathX = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\xCoor.txt"
+    xFile = open(filepathX, "r+")
+    filepathtY = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\yCoor.txt"
+    yFile = open(filepathtY, "r+")
+    x = int(xFile.read())
+    y = int(yFile.read())
+    xInt = round(inter(x, 0, 1280, 0, 640))
+    yInt = round(inter(y, 0, 720, 0, 480))
+    dist = depth.get_distance(xInt, yInt)
+    print(dist)
--- venv/Modules/qrDetection.py
+++ venv/Modules/qrDetection.py
@@ -2,12 +2,26 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 import pandas as pd
+import pyrealsense2 as rs
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
+#Start RealSense pipeline and save depth frames
+pipeline = rs.pipeline()
+pipeline.start()
+frames = pipeline.wait_for_frames()
+depth = frames.get_depth_frame()
+
+#Interpolation function
+def inter(Input, InputLow, InputHigh, OutputLow, OutputHigh):
+    interpolate = ((Input - InputLow) / (InputHigh - InputLow)) * (OutputHigh - OutputLow) + OutputLow
+    return (interpolate)
+
+
 while True:
+    #QR Reading code
     success, img = cap.read()
     for barcode in decode(img):
         myData = barcode.data.decode('utf-8')
@@ -19,18 +33,22 @@ while True:
         pts2 = barcode.rect
         cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX,
                     0.9,(255,0,255),1)
+        #pts2 contains the x,y coordinates of the corner of the QR code and the width of each side
         x = int(pts2[0])
         y = int(pts2[1])
         xW = int(pts2[2])
         yW = int(pts2[3])
-        
+        #Add half of the width to get center coordinates
         xOut = x + xW // 2
         yOut = y + yW // 2
 
+        #Open files to store coordinates
         filepathX = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\xCoor.txt"
         xFile = open(filepathX, "w+")
         filepathtY = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\yCoor.txt"
         yFile = open(filepathtY, "w+")
+        filepathtD = "C:\\Users\\geo_t\\PycharmProjects\\xArm\\venv\\Modules\\Docs\\dCoor.txt"
+        dFile = open(filepathtD, "w+")
 
         xFile.write(str(xOut))
         yFile.write(str(yOut))
@@ -39,5 +57,18 @@ while True:
         yFile.close()
 
         print(xOut, yOut)
+
+
+        #DEPTH STUFF
+        xDe = round(inter(xOut, 0, 1280, 0, 640))
+        yDe = round(inter(yOut, 0, 720, 0, 480))
+        dist = depth.get_distance(xDe, yDe)
+        if dist != 0:
+            print(dist)
+            dFile.write(str(dist))
+            dFile.close()
+
+
+
     cv2.imshow('Result', img)
     cv2.waitKey(1)
